5d6c66e8d5626c5db517518f8d7aba8abc255546
Ruslan Nam <buffalo-9@bk.ru>
2021-07-14 18:28:25 +0300

    Added class Person

---
+++ src/main/java/nam/ruslan/gitoverview/Person.java
@@ -0,0 +1,31 @@
+package nam.ruslan.gitoverview;
+
+public class Person extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
